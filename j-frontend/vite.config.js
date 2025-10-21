220f11eb666fae6cef89e1e2b0707b38f3e37c2c
jilllovespink <smile871028@gmail.com>
2025-10-21 10:41:44 +0800

    FIX edit vite.config.js to change the mode

--- j-frontend/vite.config.js
+++ j-frontend/vite.config.js
@@ -1,20 +1,26 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: "/",
-  build: {
-    outDir: "dist",
-  },
-  plugins: [vue(), tailwindcss()],
-  watch: {
-    usePolling: true, // required on Windows + Docker
-    interval: 1000, // check file changes every 1s
-  },
-  server: {
-    host: "0.0.0.0", // 允許從外部（Docker、區網）訪問
-    port: 5173, // 指定 port，預設就是 5173
-  },
+
+export default defineConfig(({ mode }) => {
+  // 讀取對應的 .env 檔案（例如 .env 或 .env.production）
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    base: "/",
+    build: {
+      outDir: "dist",
+    },
+    plugins: [vue(), tailwindcss()],
+    watch: {
+      usePolling: true, // required on Windows + Docker
+      interval: 1000, // check file changes every 1s
+    },
+    server: {
+      host: "0.0.0.0", // 允許從外部（Docker、區網）訪問
+      port: 5173, // 指定 port，預設就是 5173
+    },
+  };
 });
